dabf323be47344d094ebded7e7e6f270d422cdbb
Dv <rb.davi24@gmail.com>
2026-09-16 17:33:24 -0300

    pocketrack supermega based
    appzin ficando bolado ja papai

--- my-app/src/screens/index.tsx
+++ my-app/src/screens/index.tsx
@@ -1,12 +1,90 @@
-import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Pressable, TextInput } from 'react-native';
 import { styles } from './style';
 
 export default function SignIn() {
+  const [saldo, setSaldo] = useState(500);
+  const [valorGasto, setValorGasto] = useState('');
+const [mostrarInput, setMostrarInput] = useState(false);
+const [gastos, setGastos] = useState(300);
+const [valorSaldo, setValorSaldo] = useState('');
+const [valorRetirada, setValorRetirada] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Agora funcionando </Text>
-      <TextInput style={styles.input} />
-    </View>
-  );
-}
+   <View style={styles.container}>
+  <Text style={styles.title}>PocketTrack</Text>
+
+  <View style={styles.balanceCard}>
+    <Text style={styles.balanceLabel}>Saldo disponível</Text>
+    <Text style={styles.balance}>R$ {saldo},00</Text>
+  </View>
+
+  <View style={styles.row}>
+
+  <View style={styles.infoCard}>
+    <Text>Entradas</Text>
+    <Text>R$ 800,00</Text>
+  </View>
+
+  <View style={styles.infoCard}>
+    <Text>Gastos</Text>
+    <Text>R$ {gastos},00</Text>
+  </View>
+
+</View>
+{mostrarInput && (
+  <View>
+    <Text>Digite o valor do gasto:</Text>
+
+    <TextInput
+      value={valorGasto}
+      onChangeText={setValorGasto}
+      placeholder="Ex: 50"
+      keyboardType="numeric"
+    />
+
+    <Pressable onPress={() => {
+      const valor = Number(valorGasto);
+
+      setSaldo(saldo - valor);
+      setGastos(gastos + valor);
+
+      setValorGasto('');
+      setMostrarInput(false);
+    }}>
+      <Text>Confirmar gasto</Text>
+    </Pressable>
+  </View>
+)}
+
+<TextInput
+  value={valorSaldo}
+  onChangeText={setValorSaldo}
+  placeholder="Adicionar saldo"
+  keyboardType="numeric"
+/>
+
+<Pressable onPress={() => {
+  const valor = Number(valorSaldo);
+
+  setSaldo(saldo + valor);
+  setValorSaldo('');
+}}>
+  <Text>+ Adicionar saldo</Text>
+</Pressable>
+<TextInput
+  value={valorRetirada}
+  onChangeText={setValorRetirada}
+  placeholder="Tirar saldo"
+  keyboardType="numeric"
+/>
+
+<Pressable onPress={() => {
+  const valor = Number(valorRetirada);
+
+  setSaldo(saldo - valor);
+  setValorRetirada('');
+}}>
+  <Text>- Tirar saldo</Text>
+</Pressable>
+  </View>
+)}
--- my-app/src/screens/style.ts
+++ my-app/src/screens/style.ts
@@ -1,15 +1,42 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    paddingTop: 60,
   },
-  input: {
-    height: 50,
-    width: 200,
-    borderBottomWidth: 2,
-    borderBottomColor: 'white',
-  }
-})
+
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 25,
+  },
+
+  balanceCard: {
+    padding: 25,
+    borderRadius: 15,
+    marginBottom: 15,
+  },
+
+  balanceLabel: {
+    fontSize: 16,
+  },
+
+  balance: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+
+  row: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+
+  infoCard: {
+    flex: 1,
+    padding: 20,
+    borderRadius: 15,
+  },
+});
